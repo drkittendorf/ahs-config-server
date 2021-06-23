6f7101cade540573c39ac8c9822b4f57fca7ffca
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-06-23 13:25:04 -0400

    added initialze files

---
+++ src/main/java/com/adhocsensei/ahsconfigserver/AhsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.adhocsensei.ahsconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhsConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adhocsensei/ahsconfigserver/AhsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhocsensei.ahsconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
